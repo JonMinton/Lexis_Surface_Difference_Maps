cfb9482ac0c4e14b711b71d00209f54673f6fdd3
Jonathan Minton <nate.minton@gmail.com>
2015-10-03 16:56:50 +0100

    correct superfluous underscore (GBR_CENW should be GBRCENW) and create appendix image comparing Scotland with England & Wales and Northern Ireland separately

--- scripts/final_figures_for_scot_in_context.R
+++ scripts/final_figures_for_scot_in_context.R
@@ -204,7 +204,7 @@ dta_scot <- dta %>%
 
 dta_rUK <- dta %>% 
   filter(
-    country %in% c("GBR_CENW", "GBR_NIR"),
+    country %in% c("GBRCENW", "GBR_NIR"),
     year >= 1950 & year <= 2010,
     sex !="total"
   ) %>% 
@@ -224,7 +224,7 @@ dta_rUK <- dta %>%
 
 dta_uk <- dta %>% 
   filter(
-    country %in% c("GBR_SCO", "GBR_CENW", "GBR_NIR"),
+    country %in% c("GBR_SCO", "GBRCENW", "GBR_NIR"),
     year >= 1950 & year <= 2010,
     sex != "total"
   ) %>% 
@@ -569,3 +569,162 @@ dev.off()
 
 
   
+
+
+# Appendix ----------------------------------------------------------------
+
+
+# Scotland compared with England & Wales
+# Scotland compared with Northern Ireland
+
+
+
+
+# Figure 2 : composite: CLP Scot - rUK, Scot - rWE, UK - rWE ---------------
+
+dta_scot <- dta %>% 
+  filter(
+    country == "GBR_SCO",
+    year >= 1950 & year <= 2010, 
+    sex != "total"
+  ) %>% 
+  mutate(
+    death_rate = death_count / population_count,
+    lg_death_rate = log(death_rate, base = 10)
+  )
+
+dta_enw <- dta %>% 
+  filter(
+    country %in% c("GBRCENW"),
+    year >= 1950 & year <= 2010,
+    sex !="total"
+  ) %>% 
+  group_by(
+    sex, year, age
+  ) %>% 
+  summarise(
+    death_count = sum(death_count, na.rm = T), 
+    population_count = sum(population_count, na.rm=T)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    death_rate = death_count / population_count,
+    lg_death_rate = log(death_rate, base = 10)
+  )
+
+
+dta_nir <- dta %>% 
+  filter(
+    country %in% c("GBR_NIR"),
+    year >= 1950 & year <= 2010,
+    sex !="total"
+  ) %>% 
+  group_by(
+    sex, year, age
+  ) %>% 
+  summarise(
+    death_count = sum(death_count, na.rm = T), 
+    population_count = sum(population_count, na.rm=T)
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    death_rate = death_count / population_count,
+    lg_death_rate = log(death_rate, base = 10)
+  )
+
+
+
+
+# Difference between scotland and england & wales
+tmp1 <- dta_scot %>% 
+  select(year, age, sex, lg_death_rate) %>% 
+  rename(Scotland = lg_death_rate )
+
+tmp2 <- dta_enw  %>% 
+  select(year, age, sex, lg_death_rate) %>% 
+  rename(`England & Wales` = lg_death_rate)
+
+dif_scot_enw <- tmp1 %>% 
+  left_join(tmp2) %>% 
+  mutate(
+    `Scotland less England & Wales` = Scotland - `England & Wales`
+  ) %>% 
+  select(
+    year, age, sex, `Scotland less England & Wales`
+  )
+
+rm(tmp1, tmp2)
+
+
+# Difference between scotland and Northern Ireland
+tmp1 <- dta_scot %>% 
+  select(year, age, sex, lg_death_rate) %>% 
+  rename(Scotland = lg_death_rate )
+
+tmp2 <- dta_nir  %>% 
+  select(year, age, sex, lg_death_rate) %>% 
+  rename(`Northern Ireland` = lg_death_rate)
+
+dif_scot_nir <- tmp1 %>% 
+  left_join(tmp2) %>% 
+  mutate(
+    `Scotland less Northern Ireland` = Scotland - `Northern Ireland`
+  ) %>% 
+  select(
+    year, age, sex, `Scotland less Northern Ireland`
+  )
+
+rm(tmp1, tmp2)
+
+
+
+# Join the above together
+
+comparisons <- dif_scot_enw %>% 
+  left_join(dif_scot_nir) %>% 
+  gather(key = "comparison", value = "dif_logs", -year, -age, -sex)
+
+
+
+png(filename="figures/scotland_in_context/final_figures/figure_A01_clp_scot_enw_nir.png", 
+    width=30, height=30, res=300, units="cm"
+)
+
+comparisons %>% 
+  mutate(
+    dif_logs = ifelse(dif_logs < -0.30, -0.30, dif_logs),
+    dif_logs = ifelse(dif_logs > 0.30, 0.30, dif_logs)
+  ) %>%   
+  smooth_var(., 
+             group_vars = c("sex", "comparison"), 
+             smooth_var = "dif_logs", 
+             smooth_par = 0.7
+  ) %>% 
+  levelplot(
+    dif_logs ~ year * age | comparison + sex,
+    data=., 
+    region=T,
+    ylab="Age in years",
+    xlab="Year",
+    at = seq(from= -0.30, to = 0.30, by=0.01),
+    col.regions = colorRampPalette(rev(brewer.pal(6, "RdBu")))(200),
+    scales=list(alternating=3),
+    main=NULL,
+    aspect= "iso",
+    ylim=c(0, 90),
+    xlim=c(1950, 2010),
+    par.settings=list(strip.background=list(col="lightgrey")), 
+    panel = function(x, y, z, ...){
+      panel.levelplot(x, y, z, ...)
+      panel.abline(a = -1920, b = 1, col = "black")
+      panel.abline(a = -1950, b = 1, col = "black")
+      panel.abline(a = -1960, b = 1, col ="black")
+      panel.abline(v = c(1970, 1990, 2000), col = "black")
+      panel.abline(h = 18, col = "black")
+    }
+  )
+
+dev.off()
+
+
+
