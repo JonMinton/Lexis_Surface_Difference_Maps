4ccf8a06c7438055ed2e03f058fe3155280630b0
Jonathan Minton <nate.minton@gmail.com>
2015-12-11 14:58:37 +0000

    added additional causes of death to usa exploration

--- scripts/case_deaton_data_management.R
+++ scripts/case_deaton_data_management.R
@@ -36,6 +36,82 @@ require(spatstat)
 require(xtable)
 
 
+# full range is now: 
+# Alcohol induced [done]
+# all cause mortality [done]
+# assault mortality [done]
+# chronic liver disease [done]
+# drug induced [done]
+# legal intervention [done]
+# motor vehicle accidents 
+# poisonings [done]
+# suicides [done]
+# vehicle transport [done]
+
+
+alcohol_induced <- read_csv(
+  "data/usa_multiple_cause/alcohol_induced_1999_2014.csv",
+  col_types = "iciccii"
+)
+
+names(alcohol_induced) <- 
+  c(
+    "year", "sex", "age", "race", "hispanic", 
+    "alcohol_induced",
+    "population_1"
+  )
+
+
+assault_homicide <- read_csv(
+  "data/usa_multiple_cause/assault_homicide_1999_2014.csv",
+  col_types = "iciccii"
+)
+
+names(assault_homicide) <- 
+  c(
+    "year", "sex", "age", "race", "hispanic", 
+    "assault_homicide", 
+    "population_2"
+  )
+  
+
+drug_induced <- read_csv(
+  "data/usa_multiple_cause/drug_induced_1999_2014.csv",
+  col_types = "iciccii"
+)
+
+names(drug_induced) <- 
+  c(
+    "year", "sex", "age", "race", "hispanic",
+    "drug_induced", 
+    "population_3"
+    )
+  
+legal_intervention <- read_csv(
+  "data/usa_multiple_cause/legal_intervention_1999_2014.csv",
+  col_types = "iciccii"
+)
+
+names(legal_intervention) <- 
+  c(
+    "year", "sex", "age", "race", "hispanic",
+    "legal_intervention", 
+    "population_4"
+  )
+
+motor_accidents <- read_csv(
+  "data/usa_multiple_cause/motor_vehicle_accidents_1999_2014.csv",
+  col_types = "iciccii"
+)
+
+names(motor_accidents) <- 
+  c(
+    "year", "sex", "age", "race", "hispanic", 
+    "motor_accidents",
+    "population_5"
+  )
+
+
 vehicle_transport <- read_csv(
   "data/usa_multiple_cause/vehicle_transport_1999_2013.csv",
   col_types = "iicccii"
@@ -45,7 +121,7 @@ names(vehicle_transport) <-
   c(
       "year", "age", "sex", "race", "hispanic", 
       "vehicle_transport",
-      "population_1"
+      "population_6"
   )
 
 
@@ -55,7 +131,7 @@ names(suicides) <-
   c(
     "year", "age", "sex", "race", "hispanic", 
     "suicides",
-    "population_2"
+    "population_7"
   )
 
 
@@ -64,7 +140,7 @@ names(poisonings) <-
   c(
     "year", "age", "sex", "race", "hispanic", 
     "poisonings",
-    "population_3"
+    "population_8"
   )
 
 
@@ -73,8 +149,11 @@ names(chronic_liver_disease) <-
   c(
     "year", "age", "sex", "race", "hispanic", 
     "chronic_liver_disease",
-    "population_4"
+    "population_9"
   )
+
+
+
 
 
 all_cause <- read_csv("data/usa_multiple_cause/all_cause_mortality_1999_2013.csv", col_types = "iicccii")
@@ -82,33 +161,43 @@ names(all_cause) <-
   c(
     "year", "age", "sex", "race", "hispanic", 
     "all_cause",
-    "population_5"
+    "population"
   )
 
 
+
+
+
 merged <- all_cause %>% 
-  full_join(chronic_liver_disease) %>% 
+  full_join(alcohol_induced) %>% 
+  full_join(assault_homicide) %>%
+  full_join(chronic_liver_disease) %>%
+  full_join(drug_induced) %>% 
+  full_join(legal_intervention) %>% 
   full_join(poisonings) %>% 
   full_join(suicides) %>% 
+  full_join(motor_accidents) %>% 
   full_join(vehicle_transport)
 
-merged <-  merged %>% mutate(
-  population = ifelse(!is.na(population_1), population_1, NA),
-  population = ifelse(is.na(population), population_2, population),
-  population = ifelse(is.na(population), population_3, population),
-  population = ifelse(is.na(population), population_4, population),
-  population = ifelse(is.na(population), population_5, population)
-)
+# merged <-  merged %>% mutate(
+#   population = ifelse(!is.na(population_1), population_1, NA),
+#   population = ifelse(is.na(population), population_2, population),
+#   population = ifelse(is.na(population), population_3, population),
+#   population = ifelse(is.na(population), population_4, population),
+#   population = ifelse(is.na(population), population_5, population)
+# )
 
 merged <- merged %>% filter(!is.na(population)) %>% 
   select(
     year, age, sex, race, hispanic, 
     chronic_liver_disease, poisonings, suicides, vehicle_transport,
+    alcohol_induced, assault_homicide, drug_induced, legal_intervention,
+    motor_accidents, 
     all_cause, population
   )
 
 tmp <- merged
-tmp[,c(6:10)] <- apply(tmp[,c(6:10)], 2, function(x) {y <- x; y[is.na(y)] <- 0; return(y)})
+tmp[,c(6:16)] <- apply(tmp[,c(6:16)], 2, function(x) {y <- x; y[is.na(y)] <- 0; return(y)})
     
 merged <- tmp
 
