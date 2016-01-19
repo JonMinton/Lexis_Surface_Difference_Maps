197eedeb899f3228ae08970af849eb1c1929313f
JonMinton <nate.minton@gmail.com>
2016-01-19 15:27:40 +0000

    combine assault rate data from icd 08 to icd 10

--- external_mort_script.R
+++ external_mort_script.R
@@ -60,7 +60,7 @@ require(spatstat)
 
 #Let's try to access this data
 
-dta <- read_delim("data/long_term_external_causes/Compressed Mortality, 1968-1978.txt", delim = "\t", na = "Not Applicable", col_types = paste(rep("c", 14), collapse = ""))
+dta <- read_delim("data/long_term_external_causes/legal_intervention_Compressed Mortality, 1968-1978.txt", delim = "\t", na = "Not Applicable", col_types = paste(rep("c", 14), collapse = ""))
 
 
 # "Notes"	
@@ -114,7 +114,132 @@ all_external_male_1968_1978 %>%
 
 
 dta <- read_delim("data/long_term_external_causes/external_Compressed Mortality, 1979-1998.txt", delim = "\t")
-source("scripts/manage_data.R")
 
 
-# Figures and tables -----------------
+# Homicide using ICD categories - homicide ICD 8 
+
+dta_homicide_icd08 <- read_delim("data/long_term_external_causes/homicide_icd_8_Compressed Mortality, 1968-1978.txt", delim = "\t")
+
+dta_hom_icd08_rate <- dta_homicide_icd08 %>% 
+  filter(Notes == "Total") %>% 
+  select(age = `Age Group`, sex = Gender, race = Race, year = Year, death_count = Deaths, population_count = Population) %>%
+  select(age, year, race, sex, death_count, population_count) %>% 
+  filter(!is.na(race), !is.na(sex), !is.na(year), !is.na(age), age != "Not Stated") %>%
+  mutate(year = as.integer(year)) %>% 
+  group_by(race, sex, year) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count)) %>%
+  mutate(death_rate = 100000 * death_count / population_count) 
+
+dta_hom_icd08_rate %>% 
+  ggplot(.) +
+  geom_line(aes(x = year, y = death_rate, group = race, colour = race, linetype = race)) + 
+  facet_wrap(~sex, scales = "free_y")
+
+
+
+# Homicide- ICD 9 
+
+dta_homicide_icd09 <- read_delim("data/long_term_external_causes/homicide_icd9.txt", delim = "\t")
+
+dta_hom_icd09_rate <- dta_homicide_icd09 %>% 
+  filter(Notes == "Total") %>% 
+  select(age = `Age Group`, sex = Gender, race = Race, year = Year, death_count = Deaths, population_count = Population) %>%
+  select(age, year, race, sex, death_count, population_count) %>% 
+  filter(!is.na(race), !is.na(sex), !is.na(year), !is.na(age), age != "Not Stated") %>%
+  mutate(year = as.integer(year)) %>% 
+  group_by(race, sex, year) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count)) %>%
+  mutate(death_rate = 100000 * death_count / population_count)
+
+dta_hom_icd09_rate %>% 
+  ggplot(.) +
+  geom_line(aes(x = year, y = death_rate, group = race, colour = race, linetype = race)) + 
+  facet_wrap(~sex, scales = "free_y")
+
+
+
+# Homicide - ICD 10 
+
+#  Can't find a homicide category so looking at assault only 
+
+dta_assault_icd10 <- read_delim("data/long_term_external_causes/assault_icd10_hisprace.txt", delim = "\t")
+
+dta_assault_icd10_rate <- dta_assault_icd10 %>% 
+  select(age = `Age Group`, year = Year, sex = Gender, race = Race, hispanic = `Hispanic Origin`, death_count = Deaths, population_count = Population) %>%
+  filter(race = !is.na(race), age = !is.na(age), year = !is.na(year), sex = !is.na(sex)) %>%
+  mutate(
+    group = NA,
+    group = ifelse(race == "White" & hispanic == "Not Hispanic or Latino", "White Non-Hispanic", group),
+    group = ifelse(race == "Black or African American" & hispanic == "Not Hispanic or Latino", "Black Non-Hispanic", group),
+    group = ifelse(hispanic == "Hispanic or Latino", "Hispanic", group)
+  ) %>% 
+  filter(!is.na(group)) %>% 
+  select(sex, race = group, age, year, death_count, population_count) %>% 
+  mutate(population_count = as.numeric(population_count)) %>% 
+  filter(population_count = !is.na(population_count)) %>% 
+  group_by(sex, race, year) %>% 
+  summarise(death_count = sum(death_count), population_count = sum(population_count)) %>% 
+  mutate(death_rate = 100000 * death_count / population_count) 
+
+dta_assault_icd10_rate %>% 
+  ggplot() +
+  geom_line(aes(x = year, y = death_rate, group = race, colour = race, linetype = race)) + 
+  facet_wrap(~sex, scales = "free_y")
+
+
+# Group the above together 
+
+dta_hom_icd08_rate
+dta_hom_icd09_rate
+
+dta_hom_icd08_09_rate <- rbind(dta_hom_icd08_rate, dta_hom_icd09_rate)
+
+dta_hom_icd08_09_rate %>% 
+  ggplot() +
+  geom_line(aes(x = year, y = death_rate, group = race, colour = race, linetype = race)) + 
+  facet_wrap(~sex, scales = "free_y")
+
+require(car)
+tmp_10 <- dta_assault_icd10_rate %>% 
+  select(r1 = race, sex, year, death_count, population_count, death_rate) %>% 
+  mutate(
+    r2 = recode(
+      r1, 
+      "
+      'Black Non-Hispanic' = 'Black';
+      'White Non-Hispanic' = 'White';
+      'Hispanic' = 'Hispanic/Other'
+      "
+    )
+  ) %>% ungroup() %>% 
+  select(race = r2, sex, year, death_count, population_count, death_rate) 
+
+
+
+
+tmp_08_09 <- dta_hom_icd08_09_rate %>%
+  select(r1 = race, sex, year, death_count, population_count, death_rate) %>% 
+  mutate(
+    r2 = recode(
+      r1, 
+      "
+      'Black or African American' = 'Black';
+      'White' = 'White';
+      'Other Race' = 'Hispanic/Other'
+      "
+    )
+    ) %>% ungroup() %>%
+  select(race = r2, sex, year, death_count, population_count, death_rate) 
+  
+
+homassault_icd_08_09_10_rates <- rbind(tmp_08_09, tmp_10)
+
+
+homassault_icd_08_09_10_rates %>% 
+  ggplot() +
+  geom_line(aes(x = year, y = death_rate, group = race, colour = race, linetype = race)) + 
+  facet_wrap(~sex, scales = "free_y") + 
+  geom_vline(xintercept = c(1979, 1999))
+
+
+
